70f791333dc0376ece37a2f5b45a3b9bad43c2d5
DanielHabenicht <daniel-habenicht@outlook.de>
2025-04-29 20:40:21 +0200

    fix mdns ip address

--- components/mdns/src/lib.rs
+++ components/mdns/src/lib.rs
@@ -9,10 +9,13 @@ use std::collections::HashMap;
 use std::future;
 use std::{future::Future, pin::Pin, str};
 use tokio::sync::broadcast::{Receiver, Sender};
+use local_ip_address::list_afinet_netifas;
 
 #[derive(Clone, Deserialize, Debug)]
 pub struct MdnsConfig {
     pub disabled: Option<bool>,
+    pub ip_addresses: Option<Vec<String>>,
+    pub hostname: Option<String>,
 }
 
 config_template!(mdns, Option<MdnsConfig>, NoConfig, NoConfig, NoConfig);
@@ -52,9 +55,22 @@ impl Module for Default {
         Box::pin(async move {
             match get_mac_address() {
                 Ok(Some(ma)) => {
+                    let network_interfaces = list_afinet_netifas().unwrap();
+                    println!("NETWORK:");
+
+                    // TODO: Find out which one to use
+
+                    for (name, ip) in network_interfaces.iter() {
+                        println!("{}:\t{:?}", name, ip);
+                    }
+
                     let mac_hex = ma.to_string().replace(":", "").to_uppercase();
-                    
-                    let responder = libmdns::Responder::new().unwrap();
+                    let vec: Vec<std::net::IpAddr> = vec![
+                        "192.168.178.33".parse::<std::net::Ipv4Addr>().unwrap().into(),
+                        // std::net::Ipv6Addr::new(0, 0, 0, 0xfe80, 0x1ff, 0xfe23, 0x4567, 0x890a).into(),
+                    ];
+                    // let (responder, _) = libmdns::Responder::with_default_handle_and_ip_list_and_hostname(vec, "".to_string()).unwrap();
+                    let responder = libmdns::Responder::new_with_ip_list(vec).unwrap();
 
                     let svc_name = config.oshome.name;
                     let friendly_name = config.oshome.friendly_name.unwrap_or(svc_name.clone());
