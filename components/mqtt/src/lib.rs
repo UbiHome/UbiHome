cd24428b10693d630f5e0496c778aa8b66b63bb7
DanielHabenicht <daniel-habenicht@outlook.de>
2025-04-30 22:32:09 +0000

    fix compile errors

--- components/mqtt/src/lib.rs
+++ components/mqtt/src/lib.rs
@@ -1,9 +1,14 @@
-use log::{debug, error, info};
-use oshome_core::{config_template, home_assistant::sensors::Component, ChangedMessage, Module, PublishedMessage};
-use rumqttc::{AsyncClient, Event, MqttOptions, QoS};
+use log::{debug, error, info, warn};
+use oshome_core::{
+    config_template, home_assistant::sensors::Component, ChangedMessage, Module, PublishedMessage,
+};
+use rumqttc::{AsyncClient, ConnectionError, Event, MqttOptions, QoS};
 use serde::{Deserialize, Deserializer, Serialize};
 use std::{collections::HashMap, future::Future, pin::Pin, str, time::Duration};
-use tokio::{sync::broadcast::{Receiver, Sender}, time::sleep};
+use tokio::{
+    sync::broadcast::{Receiver, Sender},
+    time::sleep,
+};
 
 #[derive(Clone, Deserialize, Debug)]
 pub struct MqttConfig {
@@ -85,7 +90,7 @@ struct MqttDiscoveryMessage {
 #[derive(Clone, Debug)]
 pub struct Default {
     config: Config,
-    core: CoreConfig
+    core: CoreConfig,
 }
 
 #[derive(Clone, Deserialize, Debug)]
@@ -103,12 +108,16 @@ pub struct MqttButtonConfig {
     // pub bla: String
 }
 
-
-config_template!(mqtt, MqttConfig, MqttButtonConfig, MqttSensorConfig, MqttSensorConfig);
+config_template!(
+    mqtt,
+    MqttConfig,
+    MqttButtonConfig,
+    MqttSensorConfig,
+    MqttSensorConfig
+);
 
 impl Default {
     pub fn new(config_string: &String) -> Self {
-
         let config = serde_yaml::from_str::<Config>(config_string).unwrap();
         let core_config = serde_yaml::from_str::<CoreConfig>(config_string).unwrap();
 
@@ -120,13 +129,10 @@ impl Default {
 }
 
 impl Module for Default {
-
-
     fn validate(&mut self) -> Result<(), String> {
         Ok(())
     }
 
-    
     fn init(&mut self) -> Result<Vec<Component>, String> {
         let components: Vec<Component> = Vec::new();
 
@@ -165,17 +171,15 @@ impl Module for Default {
 
             let base_topic = format!(
                 "{}/{}",
-                config.mqtt
+                config
+                    .mqtt
                     .discovery_prefix
                     .clone()
                     .unwrap_or("os-home".to_string()),
-                    core_config.oshome.name
+                core_config.oshome.name
             );
-            let discovery_topic = format!("homeassistant/device/{}/config", core_config.oshome.name);
-
-           
-
-
+            let discovery_topic =
+                format!("homeassistant/device/{}/config", core_config.oshome.name);
 
             // Handle incoming messages
             let base_topic1 = base_topic.clone();
@@ -190,36 +194,33 @@ impl Module for Default {
                                     .split_off(base_topic1.clone().len() + 1);
                                 debug!("Received message on topic: {}", topic);
                                 // if components.contains_key(&topic) {
-                                    // let payload = str::from_utf8(&publish.payload).unwrap_or("");
-                                    let msg = ChangedMessage::ButtonPress {
-                                        key: topic.to_string(),
-                                    };
+                                // let payload = str::from_utf8(&publish.payload).unwrap_or("");
+                                let msg = ChangedMessage::ButtonPress {
+                                    key: topic.to_string(),
+                                };
 
-                                    sender.send(msg).unwrap();
-                                    debug!("Received on '{}': {:?}", topic, publish.payload);
+                                sender.send(msg).unwrap();
+                                debug!("Received on '{}': {:?}", topic, publish.payload);
                                 // }
                             }
                         }
                         Ok(Event::Outgoing(_)) => {}
-                        Err(e) => {
-                            match e {
-                                rumqttc::Error::Io(e_io) => {
-                                    match e_io {
-                                        std::io::ErrorKind::NetworkUnreachable => {
-                                            warn!("Network unreachable, trying again...");
-                                            sleep(Duration::from_secs(60))
-                                        }
-                                        _ => {
-                                            error!("Network error: {:?}", e);
-                                            break;
-                                        }
-                                    }
+                        Err(e) => match e {
+                            ConnectionError::Io(e_io) => match e_io.kind() {
+                                std::io::ErrorKind::NetworkUnreachable => {
+                                    warn!("Network unreachable, trying again...");
+                                    sleep(Duration::from_secs(60)).await
                                 }
                                 _ => {
-                                    error!("Error in MQTT event loop: {:?}", e);
+                                    error!("Network error: {:?}", e_io);
                                     break;
                                 }
-                        }
+                            },
+                            _ => {
+                                error!("Error in MQTT event loop: {:?}", e);
+                                break;
+                            }
+                        },
                     }
                 }
             });
@@ -228,32 +229,42 @@ impl Module for Default {
             let base_topic2 = base_topic.clone();
             tokio::spawn(async move {
                 while let Ok(cmd) = receiver.recv().await {
-
                     match cmd {
                         PublishedMessage::Components { components } => {
-
-                            let mut mqtt_components: HashMap<String, MqttComponent> = HashMap::new();
+                            let mut mqtt_components: HashMap<String, MqttComponent> =
+                                HashMap::new();
                             let mut topics: Vec<String> = vec![];
 
                             for component in components {
                                 match component {
                                     Component::Button(button) => {
-                                        let id = button.unique_id.unwrap_or(format!("{}_{}", core_config.oshome.name, button.name));
-                                        let topic = format!("{}/{}", base_topic.clone(), id.clone());
+                                        let id = button.unique_id.unwrap_or(format!(
+                                            "{}_{}",
+                                            core_config.oshome.name, button.name
+                                        ));
+                                        let topic =
+                                            format!("{}/{}", base_topic.clone(), id.clone());
                                         topics.push(topic.clone());
                                         mqtt_components.insert(
                                             id.clone(),
                                             MqttComponent::Button(HAMqttButton {
                                                 platform: "button".to_string(),
                                                 unique_id: id.clone(),
-                                                command_topic: format!("{}/{}", base_topic.clone(), button.object_id.clone()),
+                                                command_topic: format!(
+                                                    "{}/{}",
+                                                    base_topic.clone(),
+                                                    button.object_id.clone()
+                                                ),
                                                 name: button.name.clone(),
                                                 object_id: button.object_id.clone(),
                                             }),
                                         );
                                     }
                                     Component::Sensor(sensor) => {
-                                        let id = sensor.unique_id.unwrap_or(format!("{}_{}", core_config.oshome.name, sensor.name));
+                                        let id = sensor.unique_id.unwrap_or(format!(
+                                            "{}_{}",
+                                            core_config.oshome.name, sensor.name
+                                        ));
                                         mqtt_components.insert(
                                             id.clone(),
                                             MqttComponent::Sensor(HAMqttSensor {
@@ -261,15 +272,24 @@ impl Module for Default {
                                                 icon: sensor.icon.clone(),
                                                 unique_id: id.clone(),
                                                 device_class: sensor.device_class.clone(),
-                                                unit_of_measurement: sensor.unit_of_measurement.clone(),
+                                                unit_of_measurement: sensor
+                                                    .unit_of_measurement
+                                                    .clone(),
                                                 name: sensor.name.clone(),
-                                                state_topic: format!("{}/{}", base_topic.clone(), sensor.object_id.clone()),
+                                                state_topic: format!(
+                                                    "{}/{}",
+                                                    base_topic.clone(),
+                                                    sensor.object_id.clone()
+                                                ),
                                                 object_id: sensor.object_id.clone(),
                                             }),
                                         );
                                     }
                                     Component::BinarySensor(sensor) => {
-                                        let id = sensor.unique_id.unwrap_or(format!("{}_{}", core_config.oshome.name, sensor.name));
+                                        let id = sensor.unique_id.unwrap_or(format!(
+                                            "{}_{}",
+                                            core_config.oshome.name, sensor.name
+                                        ));
                                         mqtt_components.insert(
                                             id.clone(),
                                             MqttComponent::BinarySensor(HAMqttBinarySensor {
@@ -278,14 +298,18 @@ impl Module for Default {
                                                 unique_id: id.clone(),
                                                 device_class: sensor.device_class.clone(),
                                                 name: sensor.name.clone(),
-                                                state_topic: format!("{}/{}", base_topic.clone(), sensor.object_id.clone()),
+                                                state_topic: format!(
+                                                    "{}/{}",
+                                                    base_topic.clone(),
+                                                    sensor.object_id.clone()
+                                                ),
                                                 object_id: sensor.object_id.clone(),
                                             }),
                                         );
                                     }
                                 }
                             }
-                
+
                             let device = Device {
                                 identifiers: vec![core_config.oshome.name.clone()],
                                 manufacturer: format!(
@@ -297,30 +321,36 @@ impl Module for Default {
                                 name: core_config.oshome.name.clone(),
                                 model: whoami::devicename(),
                             };
-                
+
                             let origin = Origin {
                                 name: "os-home".to_string(),
                                 sw: "0.1".to_string(),
                                 url: "https://test.com".to_string(),
                             };
-                
+
                             let discovery_message = MqttDiscoveryMessage {
                                 device,
                                 origin,
                                 components: mqtt_components.clone(),
                             };
-                            let discovery_payload = serde_json::to_string(&discovery_message).unwrap();
-                
+                            let discovery_payload =
+                                serde_json::to_string(&discovery_message).unwrap();
+
                             debug!("Publishing discovery message to topic: {}", discovery_topic);
                             debug!("Discovery payload: {}", discovery_payload);
-                
+
                             // ERROR [oshome_mqtt] Error in MQTT event loop: Io(Os { code: 101, kind: NetworkUnreachable, message: "Network unreachable" })
 
                             client
-                                .publish(&discovery_topic, QoS::AtLeastOnce, false, discovery_payload)
+                                .publish(
+                                    &discovery_topic,
+                                    QoS::AtLeastOnce,
+                                    false,
+                                    discovery_payload,
+                                )
                                 .await
                                 .unwrap();
-                
+
                             debug!("Discovery message published successfully");
 
                             // Subscribe to the discovery topic
