b9145ca561e634e559a5109adea1d2e7e343d9d3
DanielHabenicht <daniel-habenicht@outlook.de>
2026-02-11 19:29:19 +0000

    less ouput

--- components/sendspin/src/lib.rs
+++ components/sendspin/src/lib.rs
@@ -108,7 +108,6 @@ impl Module for UbiHomeDefault {
         let id = self.sendspin_config.id.clone().unwrap_or(name.clone());
 
         // List Hosts
-        println!("Supported hosts:\n  {:?}", cpal::ALL_HOSTS);
         let available_hosts = cpal::available_hosts();
         println!("Available hosts:\n  {available_hosts:?}");
 
@@ -116,15 +115,10 @@ impl Module for UbiHomeDefault {
             println!("{}", host_id.name());
             let host = cpal::host_from_id(host_id).unwrap();
 
-            let default_in = host
-                .default_input_device()
-                .map(|dev| dev.id().unwrap())
-                .map(|id| id.to_string());
             let default_out = host
                 .default_output_device()
                 .map(|dev| dev.id().unwrap())
                 .map(|id| id.to_string());
-            println!("  Default Input Device:\n    {default_in:?}");
             println!("  Default Output Device:\n    {default_out:?}");
 
             let devices = host.devices().unwrap();
@@ -139,51 +133,28 @@ impl Module for UbiHomeDefault {
                     println!("  {}. {id}", device_index + 1);
                 }
 
-                // Input configs
-                if let Ok(conf) = device.default_input_config() {
-                    println!("    Default input stream config:\n      {conf:?}");
-                }
-                let input_configs = match device.supported_input_configs() {
-                    Ok(f) => f.collect(),
-                    Err(e) => {
-                        println!("    Error getting supported input configs: {e:?}");
-                        Vec::new()
-                    }
-                };
-                if !input_configs.is_empty() {
-                    println!("    All supported input stream configs:");
-                    for (config_index, config) in input_configs.into_iter().enumerate() {
-                        println!(
-                            "      {}.{}. {:?}",
-                            device_index + 1,
-                            config_index + 1,
-                            config
-                        );
-                    }
-                }
-
                 // Output configs
                 if let Ok(conf) = device.default_output_config() {
                     println!("    Default output stream config:\n      {conf:?}");
                 }
-                let output_configs = match device.supported_output_configs() {
-                    Ok(f) => f.collect(),
-                    Err(e) => {
-                        println!("    Error getting supported output configs: {e:?}");
-                        Vec::new()
-                    }
-                };
-                if !output_configs.is_empty() {
-                    println!("    All supported output stream configs:");
-                    for (config_index, config) in output_configs.into_iter().enumerate() {
-                        println!(
-                            "      {}.{}. {:?}",
-                            device_index + 1,
-                            config_index + 1,
-                            config
-                        );
-                    }
-                }
+                // let output_configs = match device.supported_output_configs() {
+                //     Ok(f) => f.collect(),
+                //     Err(e) => {
+                //         println!("    Error getting supported output configs: {e:?}");
+                //         Vec::new()
+                //     }
+                // };
+                // if !output_configs.is_empty() {
+                //     println!("    All supported output stream configs:");
+                //     for (config_index, config) in output_configs.into_iter().enumerate() {
+                //         println!(
+                //             "      {}.{}. {:?}",
+                //             device_index + 1,
+                //             config_index + 1,
+                //             config
+                //         );
+                //     }
+                // }
             }
         }
 
