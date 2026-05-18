eee5e5512e54317747d42c11fff6b6ecdac7f45c
DanielHabenicht <daniel-habenicht@outlook.de>
2026-05-19 00:32:48 +0200

    format

--- documentation/src/content/docs/examples/exampleEntries.ts
+++ documentation/src/content/docs/examples/exampleEntries.ts
@@ -1,7 +1,12 @@
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
 
-export const exampleEntries = (await getCollection('docs'))
-	.filter((entry) => typeof entry.id === 'string' && entry.id.startsWith('examples/') && entry.id.split('/').length === 2)
+export const exampleEntries = (await getCollection("docs"))
+	.filter(
+		(entry) =>
+			typeof entry.id === "string" &&
+			entry.id.startsWith("examples/") &&
+			entry.id.split("/").length === 2,
+	)
 	.sort((left, right) => left.data.title.localeCompare(right.data.title))
 	.map((entry) => ({
 		href: `/${entry.id}/`,
